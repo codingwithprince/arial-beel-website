46d43b9057c187d14bd63d2d779ab73d5c410aed
Prince Ahmed <codingwithprince@gmail.com>
2021-09-28 13:43:04 +0600

    added hero and navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import './css/responsive.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+    return (
+        <div className="hero-area">
+             {/* <!--Overlay  --> */}
+            <div class="bg-darks"></div>
+            <div class="content">
+                <h2 id="bigText" data-aos="zoom-out-left">আড়িয়াল বিল</h2>
+                <p data-aos="zoom-out-right">Arial Beel is the third largest wetland in Bangladesh covering an area of
+                    136 sq. km. It is situated in Sreenagar Upazila of Munshiganj District. </p>
+                <button id="readMoreButton">Read More</button>
+                <button id="loactionButton"><i class="fas fa-map-marker-alt"></i> Location</button>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
