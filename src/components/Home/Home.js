dce32706a806b1b2bd2147c76f94300ee4a10908
Prince Ahmed <codingwithprince@gmail.com>
2021-09-30 16:13:27 +0600

    added location section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
 import ImageGallery from '../ImageGallery/ImageGallery';
+import Location from '../Location/Location';
 import Seasons from '../Seasons/Seasons';
 
 const Home = () => {
@@ -12,6 +13,8 @@ const Home = () => {
             <Seasons />
             {/* === Image Gallery Section === */}
             <ImageGallery />
+            {/* === Location section === */}
+            <Location />
         </div>
     );
 };
