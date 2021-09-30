1266dc3c0c9ef9bd842bb60fe90ffdd358d61990
Prince Ahmed <codingwithprince@gmail.com>
2021-09-30 20:19:51 +0600

    some thing was changed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,11 @@ import './Hero.css';
 
 const Hero = () => {
     return (
-        <div className="hero-area header-area">
+        <div className="hero-area header-area mt-5 pt-5">
              {/* <!--Overlay  --> */}
-            <div class="bg-darks"></div>
             <div class="content">
                 <h2 id="bigText" data-aos="zoom-out-left">আড়িয়াল বিল</h2>
+              
                 <p data-aos="zoom-out-right">Arial Beel is the third largest wetland in Bangladesh covering an area of
                     136 sq. km. It is situated in Sreenagar Upazila of Munshiganj District. </p>
                 <button id="readMoreButton">Read More</button>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ import Seasons from '../Seasons/Seasons';
 
 const Home = () => {
     return (
-        <div className="home">
+        <div className="home section-padding">
             {/* === Hero Section === */}
             <Hero />
             {/* === Seasons Section === */}
